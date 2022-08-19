d506bae1f4d7438464e30edd2ea4e1ab354597f6
Tingyeu Yang <ytingyeu@asu.edu>
2022-08-19 00:22:39 -0700

    add get maze(s) methods

--- ValantDemoApi/ValantDemoApi/Controllers/MazeController.cs
+++ ValantDemoApi/ValantDemoApi/Controllers/MazeController.cs
@@ -1,24 +1,46 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using ValantDemoApi.Models;
 
 namespace ValantDemoApi.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class MazeController : ControllerBase
-    {
-        private readonly ILogger<MazeController> _logger;
+  //[ApiController]
+  [Route("[controller]")]
+  public class MazeController : ControllerBase
+  {
+    private readonly ILogger<MazeController> _logger;
+    private readonly ApiContext _context;
 
-        public MazeController(ILogger<MazeController> logger)
-        {
-            _logger = logger;
-        }
+    public MazeController(ILogger<MazeController> logger, ApiContext context)
+    {
+      _logger = logger;
+      _context = context;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Maze>> GetMazeById(int id)
+    {
+      var maze = await _context.Mazes.FindAsync(id);
+
+      if (maze == null)
+      {
+        return NotFound();
+      }
 
         [HttpGet]
         public IEnumerable<string> GetNextAvailableMoves()
         {
           return new List<string> {"Up", "Down", "Left", "Right"};
         }
+      return maze;
     }
+
+    [HttpGet("all")]
+    public ActionResult<Maze> GetAllMazes()
+    {
+      return Ok(_context.Mazes);
+    }
+  }
 }
