b8d21642000e83c6a54c05860091d06a636fafad
Tingyeu Yang <ytingyeu@asu.edu>
2022-08-20 16:37:44 -0700

    create now-loading comp.

---
+++ apps/demo/src/app/now-loading/now-loading.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'valant-now-loading',
+  templateUrl: './now-loading.component.html',
+  styleUrls: ['./now-loading.component.less']
+})
+export class NowLoadingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
