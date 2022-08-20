5fb75a6a6f161ee6e46b54194d0e8a6ae051c159
Tingyeu Yang <ytingyeu@asu.edu>
2022-08-19 19:44:53 -0700

    add file upload form and integrate with service

--- apps/demo/src/app/available-mazes/available-mazes.component.ts
+++ apps/demo/src/app/available-mazes/available-mazes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { IMaze } from '../_models/maze/maze';
 import { MazeService } from '../_services/maze.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { callbackify } from 'util';
 
 @Component({
   selector: 'valant-available-mazes',
@@ -9,14 +11,47 @@ import { MazeService } from '../_services/maze.service';
 })
 export class AvailableMazesComponent implements OnInit {
   listOfMazes: IMaze[] = [];
+  isLoaded: boolean = false;
+  uploadForm: FormGroup;
+  jsonFile: string;
 
-  constructor(private mazeService: MazeService) {}
+  constructor(private mazeService: MazeService, private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
-    this.getMazes();
+    this.uploadForm = this.formBuilder.group({
+      mazeJson: [''],
+    });
+
+    this.mazeService.getListOfMazes().subscribe((mazes) => {
+      this.listOfMazes = mazes;
+      this.isLoaded = true;
+    });
   }
 
-  getMazes() {
-    this.mazeService.getListOfMazes().subscribe((mazes) => (this.listOfMazes = mazes));
+  onFileSelect(event: { target: { files: string | any[] } }) {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.uploadForm.get('mazeJson').setValue(file);
+      this.jsonFile = file;
+    }
+  }
+
+  onSubmit() {
+    this.isLoaded = false;
+    const file = this.uploadForm.get('mazeJson').value;
+
+    const fileReader = new FileReader();
+
+    fileReader.onload = (fileLoadedEvent) => {
+      const textFromFileLoaded = fileLoadedEvent.target.result;
+      const json = JSON.parse(textFromFileLoaded.toString());
+
+      this.mazeService.postMaze(json).subscribe((mazes) => {
+        this.listOfMazes = mazes;
+        this.isLoaded = true;
+      });
+    };
+
+    fileReader.readAsText(file, 'UTF-8');
   }
 }
