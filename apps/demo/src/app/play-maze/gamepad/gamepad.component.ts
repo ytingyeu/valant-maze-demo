bcdc2509fd65e3f3cbb24d5efc716b48dcaa4619
Tingyeu Yang <ytingyeu@asu.edu>
2022-08-21 03:18:59 -0700

    impelment gamepad and control

--- apps/demo/src/app/play-maze/gamepad/gamepad.component.ts
+++ apps/demo/src/app/play-maze/gamepad/gamepad.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { IMovement } from '../../_models/maze/maze';
 
 @Component({
   selector: 'valant-gamepad',
   templateUrl: './gamepad.component.html',
-  styleUrls: ['./gamepad.component.less']
+  styleUrls: ['./gamepad.component.less'],
 })
 export class GamepadComponent implements OnInit {
+  @Input() availableMoves: IMovement[];
+  @Output() updateSelectedMove = new EventEmitter<IMovement>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onMoveClick(event: any) {
+    this.updateSelectedMove.emit(event.target.name);
   }
-
 }
--- apps/demo/src/app/play-maze/play-maze.component.ts
+++ apps/demo/src/app/play-maze/play-maze.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { IMaze } from '../_models/maze/maze';
+import { Component, Input, OnInit } from '@angular/core';
+import { ICell, IMaze, IMovement } from '../_models/maze/maze';
 import { ActivatedRoute } from '@angular/router';
 import { MazeService } from '../_services/maze.service';
 import { LoggingService } from '../logging/logging.service';
@@ -11,24 +11,62 @@ import { LoggingService } from '../logging/logging.service';
 })
 export class PlayMazeComponent implements OnInit {
   maze: IMaze | undefined;
+  graph: string[][];
   isLoaded: boolean = false;
+  currPos: ICell;
+  availableMoves: IMovement[];
 
   constructor(private logger: LoggingService, private route: ActivatedRoute, private mazeService: MazeService) {}
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.getMaze(id);
+    this.initMaze(id);
   }
 
-  private getMaze(id: number): void {
+  private initMaze(id: number): void {
     this.mazeService.getMazeById(id).subscribe({
       next: (maze: IMaze) => {
+        // console.log(maze);
         this.maze = maze;
-        this.isLoaded = true;
+        this.graph = maze.graph;
+        this.currPos = {
+          row: maze.start.row,
+          col: maze.start.col,
+        };
+
+        this.getNextAvailableMoves();
       },
       error: (error) => {
         this.logger.error('Error getting mazes: ', error);
       },
     });
+  }
+
+  private getNextAvailableMoves() {
+    this.mazeService.getNextAvailableMoves(this.maze.id, this.currPos).subscribe({
+      next: (resNextMoves: IMovement[]) => {
+        this.availableMoves = resNextMoves;
+        this.isLoaded = true;
+      },
+      error: (error) => {
+        this.logger.error('Error getting next availabe moves: ', error);
+      },
+    });
+  }
+
+  updateSelectedMove(nextMoveName: string) {
+    console.log(`Parent receives ${nextMoveName}`);
+    const movement = this.availableMoves.find((movement) => movement.name === nextMoveName);
+    console.log(movement);
+
+    const newPos = {
+      row: this.currPos.row + movement.direction.row,
+      col: this.currPos.col + movement.direction.col,
+    };
+
+    this.graph[this.currPos.row][this.currPos.col] = 'O';
+    this.graph[newPos.row][newPos.col] = 'C';
+    this.currPos = newPos;
+    this.getNextAvailableMoves();
   }
 }
