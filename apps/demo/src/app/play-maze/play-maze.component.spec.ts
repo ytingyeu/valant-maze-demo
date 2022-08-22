5f59a2e5edaf628d5eb3ba096110bf5a1f5a338f
Tingyeu Yang <ytingyeu@asu.edu>
2022-08-22 02:11:31 -0700

    fix no provider issues

--- apps/demo/src/app/play-maze/play-maze.component.spec.ts
+++ apps/demo/src/app/play-maze/play-maze.component.spec.ts
@@ -1,16 +1,32 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { IMovement } from '../_models/maze/maze';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 
 import { PlayMazeComponent } from './play-maze.component';
+import { ValantDemoApiClient } from '../api-client/api-client';
 
-describe('PlayMazeComponent', () => {
+fdescribe('PlayMazeComponent', () => {
   let component: PlayMazeComponent;
   let fixture: ComponentFixture<PlayMazeComponent>;
 
+  @Component({
+    selector: 'valant-gamepad',
+    template: '',
+  })
+  class MockGamepadComponent {
+    @Input() availableMoveNames: string[];
+    @Output() updateSelectedMove = new EventEmitter<IMovement>();
+    @Output() resetGame = new EventEmitter();
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PlayMazeComponent ]
-    })
-    .compileComponents();
+      declarations: [PlayMazeComponent, MockGamepadComponent],
+      imports: [RouterTestingModule, HttpClientTestingModule ],
+      providers: [ValantDemoApiClient.Client],
+    }).compileComponents();
   });
 
   beforeEach(() => {
