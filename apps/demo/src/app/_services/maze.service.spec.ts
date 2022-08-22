b501183acc1566a57191a325cf924d926c65226a
Tingyeu Yang <ytingyeu@asu.edu>
2022-08-22 02:06:53 -0700

    fix service tests

--- apps/demo/src/app/_services/maze.service.spec.ts
+++ apps/demo/src/app/_services/maze.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { MazeService } from './maze.service';
+import { ValantDemoApiClient } from '../api-client/api-client';
 
 describe('MazeService', () => {
   let service: MazeService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule ],
+      providers: [ValantDemoApiClient.Client],
+    });
     service = TestBed.inject(MazeService);
   });
 
