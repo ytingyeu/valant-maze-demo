dd28e97c0e37e9a19592b35f4476a3dfc945b60b
Tingyeu Yang <ytingyeu@asu.edu>
2022-08-21 21:06:10 -0700

    add test place holder

--- ValantDemoApi/ValantDemoApi.Tests/ValantDemoApiTests.cs
+++ ValantDemoApi/ValantDemoApi.Tests/ValantDemoApiTests.cs
@@ -9,33 +9,87 @@ using ValantDemoApi.Models;
 
 namespace ValantDemoApi.Tests
 {
-    [TestFixture]
-    public class ValantDemoApiTests
+  [TestFixture]
+  public class ValantDemoApiTests
+  {
+    private HttpClient client;
+
+    [OneTimeSetUp]
+    public void Setup()
     {
-      private HttpClient client;
+      var factory = new APIWebApplicationFactory();
+      this.client = factory.CreateClient();
+    }
 
-      [OneTimeSetUp]
-        public void Setup()
-        {
-          var factory = new APIWebApplicationFactory();
-          this.client = factory.CreateClient();
-        }
+    [Test]
+    public async Task ShouldReturnAllFourDirectionsForMovementThroughMaze()
+    {
+      var result = await this.client.GetAsync("/Maze/NextAvailableMoves");
+      result.EnsureSuccessStatusCode();
+      var content = JsonConvert.DeserializeObject<Movement[]>(await result.Content.ReadAsStringAsync());
 
-        [Test]
-        public async Task ShouldReturnAllFourDirectionsForMovementThroughMaze()
-        {
-          var result = await this.client.GetAsync("/Maze/NextAvailableMoves");
-          result.EnsureSuccessStatusCode();
-          var content = JsonConvert.DeserializeObject<Movement[]>(await result.Content.ReadAsStringAsync());
-
-          var expectContent = new Movement[] {
+      var expectContent = new Movement[] {
             new Movement("Up", new Cell(-1, 0)),
             new Movement("Down", new Cell(1, 0)),
             new Movement("Left", new Cell(0, -1)),
             new Movement("Right", new Cell(0, 1)),
           };
 
-          content.Should().BeEquivalentTo(expectContent);
-        }
+      content.Should().BeEquivalentTo(expectContent);
     }
+
+    [Test]
+    public async Task ShouldReturnAllAvailableMazes()
+    {
+      var result = await this.client.GetAsync("/Maze/all");
+      result.EnsureSuccessStatusCode();
+
+      //var content = JsonConvert.DeserializeObject<Movement[]>(await result.Content.ReadAsStringAsync());
+
+      //var expectContent = new Movement[] {
+      //      new Movement("Up", new Cell(-1, 0)),
+      //      new Movement("Down", new Cell(1, 0)),
+      //      new Movement("Left", new Cell(0, -1)),
+      //      new Movement("Right", new Cell(0, 1)),
+      //    };
+
+      //content.Should().BeEquivalentTo(expectContent);
+    }
+
+    [Test]
+    public async Task ShouldReturnMazeById()
+    {
+      var result = await this.client.GetAsync("/Maze/all");
+      result.EnsureSuccessStatusCode();
+
+      //var content = JsonConvert.DeserializeObject<Movement[]>(await result.Content.ReadAsStringAsync());
+
+      //var expectContent = new Movement[] {
+      //      new Movement("Up", new Cell(-1, 0)),
+      //      new Movement("Down", new Cell(1, 0)),
+      //      new Movement("Left", new Cell(0, -1)),
+      //      new Movement("Right", new Cell(0, 1)),
+      //    };
+
+      //content.Should().BeEquivalentTo(expectContent);
+    }
+
+    [Test]
+    public async Task ShouldCreateNewMazeAndReturnLocation()
+    {
+      var result = await this.client.GetAsync("/Maze/all");
+      result.EnsureSuccessStatusCode();
+
+      //var content = JsonConvert.DeserializeObject<Movement[]>(await result.Content.ReadAsStringAsync());
+
+      //var expectContent = new Movement[] {
+      //      new Movement("Up", new Cell(-1, 0)),
+      //      new Movement("Down", new Cell(1, 0)),
+      //      new Movement("Left", new Cell(0, -1)),
+      //      new Movement("Right", new Cell(0, 1)),
+      //    };
+
+      //content.Should().BeEquivalentTo(expectContent);
+    }
+  }
 }
