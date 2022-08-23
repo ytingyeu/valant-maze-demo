c45dd631c8732f79ea0d8065be2dc73f79d31eca
Tingyeu Yang <ytingyeu@asu.edu>
2022-08-22 23:02:11 -0700

    add available mazes test

--- apps/demo/src/app/available-mazes/available-mazes.component.spec.ts
+++ apps/demo/src/app/available-mazes/available-mazes.component.spec.ts
@@ -1,36 +1,42 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Shallow } from 'shallow-render';
 import { ValantDemoApiClient } from '../api-client/api-client';
+import { AppModule } from '../app.module';
+import { of } from 'rxjs';
 
 import { AvailableMazesComponent } from './available-mazes.component';
+import { MazeService } from '../_services/maze.service';
+import { MOCK_MAZES } from '../_models/maze/mock-mazes';
+
+const mockMazeService = {
+  getListOfMazes: jest.fn(() => of(MOCK_MAZES)),
+};
 
 describe('AvailableMazesComponent', () => {
-  let component: AvailableMazesComponent;
-  let fixture: ComponentFixture<AvailableMazesComponent>;
-
-  @Component({
-    selector: 'valant-upload-maze',
-    template: '',
-  })
-  class MockUploadComponent {}
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AvailableMazesComponent, MockUploadComponent ],
-      imports: [HttpClientTestingModule],
-      providers: [ValantDemoApiClient.Client],
-    })
-    .compileComponents();
-  });
+  let component: Shallow<AvailableMazesComponent>;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AvailableMazesComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = new Shallow(AvailableMazesComponent, AppModule)
+      // .provideMock({ provide: StuffService, useValue: mockStuffService })
+      // .provideMock({ provide: LoggingService, useClass: SilentLogger });
+      .provideMock({ provide: MazeService, useValue: mockMazeService });
+    jest.clearAllMocks();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should render available mazes', async () => {
+    const rendering = await component.render();
+    expect(rendering).toBeTruthy();
+  });
+
+  it('shoud call service to fetch mazes on init', async () => {
+    await component.render();
+    expect(mockMazeService.getListOfMazes).toHaveBeenCalledTimes(1);
+  });
+
+  it('shoud render mazes after fetch data on init', async () => {
+    const { find } = await component.render();
+    expect(find('.maze-entity')).toHaveFoundMoreThan(1);
   });
 });
