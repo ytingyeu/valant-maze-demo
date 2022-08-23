5e52b160c95173cab5426cc38a4ec4b92dde8e6b
Tingyeu Yang <ytingyeu@asu.edu>
2022-08-22 20:34:11 -0700

    add api-client test

---
+++ apps/demo/src/app/api-client/api-client.spec.ts
@@ -0,0 +1,79 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { ValantDemoApiClient } from '../api-client/api-client';
+import { MOCK_MAZES, NEW_MAZE_REQ, VALID_MOVEMENTS } from '../_models/maze/mock-mazes';
+import { IMaze, INewMaze, IMovement } from '../_models/maze/maze';
+import { HttpClient } from '@angular/common/http';
+
+fdescribe('ApiClient', () => {
+  let apiClient: ValantDemoApiClient.Client;
+  let httpMock: HttpTestingController;
+  let httpClient: HttpClient;
+  let baseUrl: string;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        {
+          provide: ValantDemoApiClient.Client,
+          useClass: ValantDemoApiClient.Client,
+        },
+        {
+          provide: ValantDemoApiClient.API_BASE_URL,
+          useValue: ValantDemoApiClient.API_BASE_URL,
+        },
+      ],
+    });
+
+    apiClient = TestBed.inject(ValantDemoApiClient.Client);
+    baseUrl = TestBed.inject(ValantDemoApiClient.API_BASE_URL);
+    httpMock = TestBed.inject(HttpTestingController);
+    httpClient = TestBed.inject(HttpClient);
+  });
+
+  it('ValantDemoApiClient should be created', () => {
+    expect(apiClient).toBeTruthy();
+  });
+
+  it('getListOfMazes should make GET requesut to fetch a list of mazes.', () => {
+    apiClient.getListOfMazes().subscribe((response) => {
+      expect(response).toEqual(MOCK_MAZES);
+    });
+
+    const req = httpMock.expectOne(`${baseUrl}/Maze/all`);
+    expect(req.request.method).toBe('GET');
+    req.flush(MOCK_MAZES);
+  });
+
+  it('getMazeById should make GET requesut to fetch a maze by Id.', () => {
+    apiClient.getMazeById(MOCK_MAZES[0].id).subscribe((response) => {
+      expect(response).toEqual(MOCK_MAZES[0]);
+    });
+
+    const req = httpMock.expectOne(`${baseUrl}/Maze/${MOCK_MAZES[0].id}`);
+    expect(req.request.method).toBe('GET');
+    req.flush(MOCK_MAZES[0]);
+  });
+
+  it('postNewMaze should make POST requesut to create a maze.', () => {
+    apiClient.postNewMaze(NEW_MAZE_REQ).subscribe((response) => {
+      expect(response).toEqual(MOCK_MAZES[0]);
+    });
+
+    const req = httpMock.expectOne(`${baseUrl}/Maze`);
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toBe(NEW_MAZE_REQ);
+    req.flush(MOCK_MAZES[0]);
+  });
+
+  it('getNextMovements should make GET requesut to fetch available moves.', () => {
+    apiClient.getNextMovements().subscribe((response) => {
+      expect(response).toEqual(VALID_MOVEMENTS);
+    });
+
+    const req = httpMock.expectOne(`${baseUrl}/Maze/NextAvailableMoves`);
+    expect(req.request.method).toBe('GET');
+    req.flush(VALID_MOVEMENTS);
+  });
+});
