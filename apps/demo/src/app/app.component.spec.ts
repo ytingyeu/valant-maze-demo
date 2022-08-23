257bb79616c65b585b898b8d9f3a9deda8f72c1b
Tingyeu Yang <ytingyeu@asu.edu>
2022-08-23 01:26:58 -0700

    remove unused test

--- apps/demo/src/app/app.component.spec.ts
+++ apps/demo/src/app/app.component.spec.ts
@@ -6,15 +6,15 @@ import { LoggingService } from './logging/logging.service';
 import { SilentLogger } from './logging/silent-logger';
 import { StuffService } from './stuff/stuff.service';
 
-const mockStuffService = { getStuff: jest.fn(() => of([])) };
+// const mockStuffService = { getStuff: jest.fn(() => of([])) };
 
 describe('AppComponent', () => {
   let component: Shallow<AppComponent>;
 
   beforeEach(() => {
-    component = new Shallow(AppComponent, AppModule)
-      .provideMock({ provide: StuffService, useValue: mockStuffService })
-      .provideMock({ provide: LoggingService, useClass: SilentLogger });
+    component = new Shallow(AppComponent, AppModule);
+    // .provideMock({ provide: StuffService, useValue: mockStuffService })
+    // .provideMock({ provide: LoggingService, useClass: SilentLogger });
     jest.clearAllMocks();
   });
 
@@ -31,10 +31,5 @@ describe('AppComponent', () => {
   it('should render a banner message', async () => {
     const { find } = await component.render();
     expect(find('h1').nativeElement.textContent).toBe('Welcome to Valant demo');
-  });
-
-  it('gets stuff from the API on init', async () => {
-    await component.render();
-    expect(mockStuffService.getStuff).toHaveBeenCalledTimes(1);
   });
 });
